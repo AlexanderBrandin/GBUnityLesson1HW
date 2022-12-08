bd5adcec8b0f34ba887043b534ce7f64c41bb464
AlexanderBrandin <azartsb@yandex.ru>
2022-12-08 13:27:04 +0300

    Lesson4
    - KeyDoor System
    - List for Key
    - Use Key and Door by press a button
    - Navigation AI

--- Assets/Scripts/KeyDoor/KeyHolder.cs
+++ Assets/Scripts/KeyDoor/KeyHolder.cs
@@ -1,26 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class KeyHolder : MonoBehaviour
 {
-    [SerializeField] private GameObject m_KeyType;
-    
+    private List<Key.KyeType> m_KeyList;
+    private Key m_Key;
+    private Door m_Door;
+
+    public bool m_Triggered = false;
+
+    private void Awake()
+    {
+        m_KeyList = new List<Key.KyeType>();
+    }
+
+    public void AddKey(Key.KyeType m_KeyType)
+    {
+        Debug.Log("Add Key: " + m_KeyType);
+        m_KeyList.Add(m_KeyType);
+    }
+
+    public void RemoveKey(Key.KyeType m_KeyType)
+    {
+        m_KeyList.Remove(m_KeyType);
+    }
+
+    public bool ContainsKey(Key.KyeType m_KeyType)
+    {
+        return m_KeyList.Contains(m_KeyType);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        Key key = other.GetComponent<Key>();
-        
-        if (key != null)
+        m_Triggered = true;
+        m_Key = other.GetComponent<Key>();
+        m_Door = other.GetComponent<Door>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        m_Triggered = false;
+        m_Key = null;
+        m_Door = null;
+    }
+    public void Use()
+    {
+        if (m_Key != null)
         {
-            
-            Destroy(key);
+            AddKey(m_Key.GetKyeType());
+            Destroy(m_Key.gameObject);
         }
 
-        Door Door = other.GetComponent<Door>();
-
-        if (Door != null)
+        if (m_Door != null)
         {
-           
-        }    
+            m_Door.UseDoor();
+
+            if (ContainsKey(m_Door.GetKyeType()))
+            {
+                RemoveKey(m_Door.GetKyeType());
+                m_Door.UnlockDoor();
+            }            
+        }
     }
 }
